ab7852aa0549e8d518573fa07e8cb15df04d4990
BI912435 <BI912435@jp.ibm.com>
2020-12-02 15:02:03 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    fp = open('/etc/hostname', 'r')
+    line = "Hello World! @" + fp.read()
+    fp.close()
+    return line
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
